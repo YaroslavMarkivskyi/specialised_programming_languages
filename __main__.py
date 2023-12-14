27f71c9eee0dcde177dcbc062153f06046d24dc0
Yaroslav Markivskyi <129275754+YaroslavMarkivskyi@users.noreply.github.com>
2023-12-14 13:07:53 +0200

    main

---
+++ __main__.py
@@ -0,0 +1,11 @@
+"""
+Module for the main entry point of the application.
+"""
+
+import sys
+
+from UI.Menu import __main__
+
+if __name__ == '__main__':
+
+    sys.exit(__main__)  # Addressed the warning by including the statement in sys.exit()
